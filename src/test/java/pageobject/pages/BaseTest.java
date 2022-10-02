38233bedd533fef078a61d3beac7ba5d06881fef
ВалÐаев Георгий <ymnikgoga@gmail.com>
2022-10-03 02:29:45 +0300

    Класс базового теста заполнен

--- src/test/java/pageobject/pages/BaseTest.java
+++ src/test/java/pageobject/pages/BaseTest.java
@@ -2,11 +2,31 @@ package pageobject.pages;
 
 import drivermanager.DriverManager;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 public abstract class BaseTest {
 
+    /**
+     * Инициализируем вебдрайвер. Подходящие имена бразуера: chrome, firefox.
+     */
+    @BeforeClass
+    public static void initChromeDriver() {
+        DriverManager.setDriver("chrome");
+    }
+
+    /**
+     * Инициализируем вебдрайвервейт
+     */
+    @BeforeClass
+    public static void initWebDriverWait() {
+        DriverManager.setDriverWait();
+    }
+
+    /**
+     * Закрываем сессию и окна браузера
+     */
     @AfterClass
-    public static void closeBrowser() {
+    public static void quitBrowser() {
         DriverManager.quitDriver();
     }
 }
