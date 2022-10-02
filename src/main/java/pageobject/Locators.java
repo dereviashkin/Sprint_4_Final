adf663ea80bc4681ca39dc5295e2489b2152a3f4
ВалÐаев Георгий <ymnikgoga@gmail.com>
2022-10-03 02:31:15 +0300

    Локаторы внесены

--- src/main/java/pageobject/Locators.java
+++ src/main/java/pageobject/Locators.java
@@ -4,11 +4,34 @@ import org.openqa.selenium.By;
 
 public class Locators {
     //Главная страница
+    //Элемент с текстом "Учебный тренажер"
+    public static final By TRAINING_SIMULATOR = By.className("Header_Disclaimer__3VEni");
+    //Кнопка принятия куков "да все привыкли"
+    public static final By ACCEPT_COOKIE = By.className("App_CookieButton__3cvqF");
     //Кнопка "Заказать" вверху страницы
-    private static final By ORDER_BUTTON_TOP = By.className("Button_Button__ra12g");
+    public static final By ORDER_BUTTON_TOP = By.className("Button_Button__ra12g");
     //Кнопка "Заказать" внизу страницы
-    private static final By ORDER_BUTTON_DOWN = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button");
-    //Список "Вопросы о важном"
-    private static final By IMPORTANT_QUESTIONS = By.cssSelector(".accordion");
+    public static final By ORDER_BUTTON_BOTTOM = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button");
+    //Список "Вопросы о важном" веб-элементы вопросов
+    public static final By FAQ_QUESTIONS = By.xpath(".//div[@class='accordion__item']");
+    //Список "Вопросы о важном" текст вопросов
+    public static final By FAQ_QUESTIONS_TEXT = By.xpath(".//div[@class='accordion']");
+    //Список "Вопросы о важном" видимые ответы
+    public static final By FAQ_ANSWER_VISIBLE = By.xpath(".//p/parent::div[not(@hidden)]");
 
+    //Страница заказа
+    //Элемент с текстом "Для кого самокат"
+    public static final By ORDER_HEADER = By.className("Order_Header__BZXOb");
+    //Строка ввода имени
+    public static final By INPUT_NAME = By.xpath(".//div/input[@placeholder='* Имя']");
+    //Строка ввода фамилии
+    public static final By INPUT_SURNAME = By.xpath(".//div/input[@placeholder='* Фамилия']");
+    //Строка ввода адреса
+    public static final By INPUT_ADDRESS = By.xpath(".//div/input[@placeholder='* Адрес: куда привезти заказ']");
+    //Выпадающее меню выбора станции метро
+    public static final By INPUT_METRO_STATION = By.xpath(".//div/input[@placeholder='* Станция метро']");
+    //Массив элементов выпадающего меню станций метро
+    public static final By INPUT_METRO_STATION_CHERKIZOVSKAYA = By.xpath(".//li[@data-index='0']");
+    //Строка ввода телефона
+    public static final By INPUT_PHONE_NUMBER = By.xpath(".//div/input[@placeholder='* Телефон: на него позвонит курьер']");
 }
