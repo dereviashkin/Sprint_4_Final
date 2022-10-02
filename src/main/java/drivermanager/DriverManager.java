60e008f78ff6de2feb0bd6073522dfad98382667
ВалÐаев Георгий <ymnikgoga@gmail.com>
2022-10-03 02:30:48 +0300

    Драйвер Менеджер заполнен

--- src/main/java/drivermanager/DriverManager.java
+++ src/main/java/drivermanager/DriverManager.java
@@ -5,28 +5,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.time.Duration;
 
 public class DriverManager {
 
     private static WebDriver driver;
+    private static WebDriverWait driverWait;
 
     /**
-     * Инициация драйвера в зависимости от переданного имени браузера
+     * Инициализация Вебдрайвера в зависимости от переданного имени браузера
      */
     private static WebDriver init(String browserName) {
         File driverPath = new File("");
-        switch (browserName){
+        switch (browserName) {
             case "chrome":
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--no-sandbox",  "--disable-dev-shm-usage");
+                chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
                 System.setProperty("webdriver.chrome.driver", driverPath.getAbsolutePath() + "\\src\\main\\resources\\webdrivers\\chromedriver.exe");
                 driver = new ChromeDriver(chromeOptions);
                 return driver;
             case "firefox":
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                //TODO перепилить аргументы под лису
                 firefoxOptions.setHeadless(true);
                 System.setProperty("webdriver.gecko.driver", driverPath.getAbsolutePath() + "\\src\\main\\resources\\webdrivers\\geckodriver.exe");
                 driver = new FirefoxDriver(firefoxOptions);
@@ -36,23 +38,22 @@ public class DriverManager {
     }
 
     /**
-     * Геттер драйвера
-     * @param browserName обозначение браузера
+     * Геттер вебдрайвера
+     *
      * @return объект драйвера
      */
-    public static WebDriver getDriver(String browserName) {
-        if (driver == null) {
-            driver = init(browserName);
-        }
+    public static WebDriver getDriver() {
         return driver;
     }
 
     /**
-     * Закрыть текущее окно бразуера
+     * Сеттер вебдрайвера
+     *
+     * @param browserName обозначение браузера
      */
-    public static void closeDriver() {
-        if (driver != null) {
-            driver.close();
+    public static void setDriver(String browserName) {
+        if (driver == null) {
+            init(browserName);
         }
     }
 
@@ -62,6 +63,34 @@ public class DriverManager {
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    /**
+     * Инициализация Вебдрайвервейта
+     *
+     * @return объект вебдрайвервейта
+     */
+    private static WebDriverWait initWait() {
+        Duration duration = Duration.ofSeconds(10);
+        return new WebDriverWait(driver, duration);
+    }
+
+    /**
+     * Геттер для вебдрайвервейта
+     *
+     * @return объект вебдрайвервейта
+     */
+    public static WebDriverWait getDriverWait() {
+        return driverWait;
+    }
+
+    /**
+     * Присваиваем ссылку объекту вебдрайвервейта
+     */
+    public static void setDriverWait() {
+        if (driverWait == null) {
+            driverWait = initWait();
         }
     }
 }
