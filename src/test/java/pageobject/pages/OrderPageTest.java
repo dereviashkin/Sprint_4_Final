aa81d2beeabb3e697f746ba9a247186a9008e773
Валаев Георгий <ymnikgoga@gmail.com>
2022-10-04 22:05:24 +0300

    Добавлен негативный кейс

--- src/test/java/pageobject/pages/OrderPageTest.java
+++ src/test/java/pageobject/pages/OrderPageTest.java
@@ -2,13 +2,23 @@ package pageobject.pages;
 
 import org.junit.Test;
 
-public class OrderPageTest extends BaseTest{
+public class OrderPageTest extends BaseTest {
 
     @Test
-    public void createNewOrderSuccess(){
+    public void createNewOrderSuccess() {
         OrderPage orderPage = new OrderPage();
         orderPage
                 .open()
-                .fullOrderForm();
+                .personalDataOrderForm("Акакий", "Башмачков", "Питер", "88005553535", true)
+                .clickNextButton()
+                .rentOrderForm();
+    }
+
+    @Test
+    public void createNewOrderFail() {
+        OrderPage orderPage = new OrderPage();
+        orderPage
+                .open()
+                .personalDataOrderForm("Акакий", "Башмачков", "Питер", "asdafdafasd", false);
     }
 }
