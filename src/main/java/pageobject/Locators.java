0413154427b3a50799f63bbcdbb340441b5de0d3
Валаев Георгий <ymnikgoga@gmail.com>
2022-10-04 22:01:44 +0300

    Добавлены локаторы для второй формы заказа

--- src/main/java/pageobject/Locators.java
+++ src/main/java/pageobject/Locators.java
@@ -7,7 +7,7 @@ public class Locators {
     //Элемент с текстом "Учебный тренажер"
     public static final By TRAINING_SIMULATOR = By.className("Header_Disclaimer__3VEni");
     //Кнопка принятия куков "да все привыкли"
-    public static final By ACCEPT_COOKIE = By.className("App_CookieButton__3cvqF");
+    public static final By ACCEPT_COOKIE_BUTTON = By.className("App_CookieButton__3cvqF");
     //Кнопка "Заказать" вверху страницы
     public static final By ORDER_BUTTON_TOP = By.className("Button_Button__ra12g");
     //Кнопка "Заказать" внизу страницы
@@ -20,7 +20,7 @@ public class Locators {
     public static final By FAQ_ANSWER_VISIBLE = By.xpath(".//p/parent::div[not(@hidden)]");
 
     //Страница заказа
-    //Элемент с текстом "Для кого самокат"
+    //Элемент с текстом заголовка страницы
     public static final By ORDER_HEADER = By.className("Order_Header__BZXOb");
     //Строка ввода имени
     public static final By INPUT_NAME = By.xpath(".//div/input[@placeholder='* Имя']");
@@ -31,7 +31,28 @@ public class Locators {
     //Выпадающее меню выбора станции метро
     public static final By INPUT_METRO_STATION = By.xpath(".//div/input[@placeholder='* Станция метро']");
     //Массив элементов выпадающего меню станций метро
-    public static final By INPUT_METRO_STATION_CHERKIZOVSKAYA = By.xpath(".//li[@data-index='0']");
+    public static final By INPUT_METRO_STATION_CHERKIZOVSKAYA = By.xpath(".//li[@data-index='1']");
     //Строка ввода телефона
     public static final By INPUT_PHONE_NUMBER = By.xpath(".//div/input[@placeholder='* Телефон: на него позвонит курьер']");
+    //Строка ввода телефона
+    public static final By INPUT_PHONE_NUMBER_ERROR_MESSAGE = By.xpath(".//div[text()='Введите корректный номер' and @class='Input_ErrorMessage__3HvIb Input_Visible___syz6']");
+    //Кнопка Далее
+    public static final By NEXT_BUTTON = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
+    //Строка ввода даты подачи самоката
+    public static final By INPUT_DATE_SCOOTER_DELIVERY = By.xpath(".//div/input[@placeholder='* Когда привезти самокат']");
+    //Строка ввода срока аренды самоката
+    public static final By INPUT_TERM_RATE = By.cssSelector(".Dropdown-arrow");
+    //Пункт выпадающего меню 1 день
+    public static final By INPUT_TERM_RATE_DAY = By.xpath(".//div[@class='Dropdown-menu']/div[text()='сутки']");
+    //Проверка установки срока аренды
+    public static final By INPUT_TERM_RATE_DAY_CHECK = By.xpath(".//div[@class='Dropdown-placeholder is-selected']");
+    //Чекбокс для черного цвета самоката
+    public static final By CHECKBOX_COLOUR = By.id("black");
+    //Строка ввода комментария для курьера
+    public static final By INPUT_COMMENT = By.xpath(".//div/input[@placeholder='Комментарий для курьера']");
+    //Кнопка подтверждения заказа
+    public static final By ORDER_BUTTON_COMPLETE = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    //Попап окно прямо совсем точного подтверждения
+    public static final By ORDER_BUTTON_ABSOLUTELY_COMPLETE = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
+
 }
