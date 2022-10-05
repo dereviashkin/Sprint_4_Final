2031566d13d99c3325ba6fb22068dca1940fd64f
Валаев Георгий <ymnikgoga@gmail.com>
2022-10-05 21:47:52 +0300

    Запилены тесты проверки FAQ согласно заданию

--- src/test/java/pageobject/pages/MainPageTest.java
+++ src/test/java/pageobject/pages/MainPageTest.java
@@ -19,10 +19,64 @@ public class MainPageTest extends BaseTest {
                 .orderButtonBottomClick();
     }
 
+    /**
+     * Тестирование FAQ с приседаниями
+     */
     @Test()
-    public void textFromFaqExpectedSuccess() {
+    public void textFaqQuestionAnswerCorrectSuccess() {
         mainPage
                 .open()
-                .getTextFromFaqList();
+                .checkTextFromFaqList();
+    }
+
+    /**
+     * Тестирование FAQ согласно заданию
+     */
+    @Test()
+    public void firstFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(1);
+    }
+
+    @Test()
+    public void secondFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(2);
+    }
+
+    @Test()
+    public void thirdFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(3);
+    }
+
+    @Test()
+    public void fourthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(4);
+    }
+
+    @Test()
+    public void fifthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(5);
+    }
+
+    @Test()
+    public void sixthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(6);
+    }
+
+    @Test()
+    public void seventhFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(7);
+    }
+
+    @Test()
+    public void eighthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.open()
+                .questionMatchesAnswer(8);
     }
 }
