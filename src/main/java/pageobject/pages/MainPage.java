af970284ec1935b3a0aa988eee824560cf3f9ed0
Валаев Георгий <ymnikgoga@gmail.com>
2022-10-06 20:14:27 +0300

    Поправлены класс тестов и класс POM

--- src/main/java/pageobject/pages/MainPage.java
+++ src/main/java/pageobject/pages/MainPage.java
@@ -20,6 +20,8 @@ public class MainPage {
     private static By trainingSimulator = By.className("Header_Disclaimer__3VEni");
     //Кнопка принятия куков "да все привыкли"
     private static By acceptCookieButton = By.className("App_CookieButton__3cvqF");
+    //Элемент окна принятия кукуков
+    private static By cookieWindows = By.className(".App_CookieConsent__1yUIN");
     //Кнопка "Заказать" вверху страницы
     private static By orderButtonTop = By.className("Button_Button__ra12g");
     //Кнопка "Заказать" внизу страницы
@@ -46,7 +48,7 @@ public class MainPage {
     private static By faqQuestionsText = By.xpath(".//div[@class='accordion']");
     //Список "Вопросы о важном" видимые ответы
     private static By faqAnswerVisible = By.xpath(".//p/parent::div[not(@hidden)]");
-    //Инициализируем список эталонов FAQ
+    //Список эталонов FAQ, элемент списка n (вопрос) соответствует элементу списка n+1 (ответ)
     List<String> idealFaqValues = List.of("Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
             "Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
             "Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
@@ -59,128 +61,134 @@ public class MainPage {
     /**
      * Метод открытия главной страницы
      */
-    public MainPage open() {
+    public MainPage openMainPage() {
         getDriver().get(mainPage);
         getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(trainingSimulator));
-        Assert.assertEquals("Похоже, главная страница не загрузилась", "УЧЕБНЫЙ ТРЕНАЖЕР", getDriver().findElement(trainingSimulator).getText());
+        return this;
+    }
+
+    /**
+     * Общий метод проверки открытия страницы
+     *
+     * @param expected ожидаемый текст заголовка
+     * @param locator  локатор заголовка на странице
+     */
+    public void checkIfPageOpened(String expected, By locator) {
+        Assert.assertEquals("Похоже, ожидаемая страница не загрузилась", expected, getDriver().findElement(locator).getText());
+    }
+
+    /**
+     * Метод проверки открытия главной страницы
+     */
+    public MainPage checkIfMainPageOpened() {
+        checkIfPageOpened("УЧЕБНЫЙ ТРЕНАЖЕР", trainingSimulator);
+        return this;
+    }
+
+    /**
+     * Метод проверки открытия страницы заказа
+     */
+    public MainPage checkIfOrderPageOpened() {
+        checkIfPageOpened("Для кого самокат", orderHeader);
+        return this;
+    }
+
+    /**
+     * Метод нажимания кнопки "принять куки"
+     */
+    public MainPage clickAcceptCookieButton() {
         if (getDriver().findElements(acceptCookieButton).size() != 0) {
             getDriver().findElement(acceptCookieButton).click();
         }
+        getDriverWait().until(ExpectedConditions.invisibilityOfElementLocated(cookieWindows));
         return this;
     }
 
     /**
      * Метод клика по верхней кнопке Заказать
      */
-    public MainPage orderButtonTopClick() {
+    public MainPage clickOrderButtonTop() {
         getDriver().findElement(orderButtonTop).click();
         getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(orderHeader));
-        Assert.assertEquals("Похоже, страница заказа не загрузилась", "Для кого самокат", getDriver().findElement(orderHeader).getText());
         return this;
     }
 
     /**
      * Метод клика по нижней кнопке Заказать
      */
-    public MainPage orderButtonBottomClick() {
+    public MainPage clickOrderButtonBottom() {
         getDriver().findElement(orderButtonBottom).click();
         getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(orderHeader));
-        Assert.assertEquals("Похоже, страница заказа не загрузилась", "Для кого самокат", getDriver().findElement(orderHeader).getText());
         return this;
     }
 
     /**
-     * Получаем список вопросов в FAQ на сайте. Хотел сделать проверку пары с сайта с ключ/значением из эталона, но
-     * ассерт по хашмапе не заработал, не успеваю допилить
+     * Метод скролла до FAQ с помощью JS
      */
-    public MainPage checkTextFromFaqList() {
-        //Скроллим до FAQ
+    public MainPage scrollFaqIntoView() {
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView();", getDriver().findElement(faqQuestions));
-        //Формируем список веб-элементов вопросов в FAQ
-        List<WebElement> elementsQuestions = getDriver().findElements(faqQuestions);
-        //Формируем текстовый список вопросов в FAQ
-        List<String> elementsQuestionsStrings = List.of(getDriver().findElement(faqQuestionsText).getText().split("\n"));
-        //Проверяем, что текст вопроса с сайта найден в нашем эталоне
-        elementsQuestionsStrings.forEach(el -> Assert.assertTrue(idealFaqValues.contains(el)));
-        for (WebElement element : elementsQuestions) {
-            //Кликаем по элементу вопроса из FAQ
-            element.click();
-            //Ждём, когда элемент ответа после клика станет видим
-            getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-            //Проверяем, что текст ответа с сайта найден в нашем эталоне
-            Assert.assertTrue(idealFaqValues.contains(getDriver().findElement(faqAnswerVisible).getText()));
-        }
         return this;
     }
 
     /**
-     * Метод сверки вопроса и ответа со страницы с эталоном, метод соответствия вопроса ответу
+     * Метод возвращает локатор по номеру вопроса
      *
-     * @param questionNumber номер вопроса со страницы, допустимые значения 1-8
+     * @param questionNumber номер вопроса
+     * @return объект локатора
      */
-    public MainPage questionMatchesAnswer(int questionNumber) {
-        //Скроллим до FAQ
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView();", getDriver().findElement(faqQuestions));
+    private By questionLocator(int questionNumber) {
         switch (questionNumber) {
             case 1:
-                //Получаем и сверяем текст вопроса с эталонным
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionOne).getText());
-                /*
-                либо вот такой вариант, но тогда локатор не в отдельном классе
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(By.xpath(".//div[@class='accordion__item'][" + questionNumber + "]")).getText());
-                 */
-                //Кликаем по вопросу, чтобы появился ответ
-                getDriver().findElement(faqQuestionOne).click();
-                //Ждём, когда элемент ответа после клика станет видим
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                //Получаем и сверяем текст ответа с ответом
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionOne;
             case 2:
-                //Стоило бы вынести всё внутри case в отдельный метод, но проблема с локаторами
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionTwo).getText());
-                getDriver().findElement(faqQuestionTwo).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionTwo;
             case 3:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionThree).getText());
-                getDriver().findElement(faqQuestionThree).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionThree;
             case 4:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionFour).getText());
-                getDriver().findElement(faqQuestionFour).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionFour;
             case 5:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionFive).getText());
-                getDriver().findElement(faqQuestionFive).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionFive;
             case 6:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionSix).getText());
-                getDriver().findElement(faqQuestionSix).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionSix;
             case 7:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionSeven).getText());
-                getDriver().findElement(faqQuestionSeven).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionSeven;
             case 8:
-                Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)), getDriver().findElement(faqQuestionEight).getText());
-                getDriver().findElement(faqQuestionEight).click();
-                getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
-                Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
-                return this;
+                return faqQuestionEight;
+            default:
+                throw new IllegalArgumentException("Номер вопроса задан некорректно: " + questionNumber + ". Допустимые значения: 1-8");
         }
-        throw new IllegalArgumentException("Заданный номер вопроса задан некорректно: " + questionNumber + ". Допустимые значения: 1-8");
+    }
+
+    /**
+     * Метод сверки полученного текста вопроса FAQ с эталоном
+     *
+     * @param questionNumber номер вопроса из FAQ
+     */
+    public MainPage realQuestionMatchesIdeal(int questionNumber) {
+        Assert.assertEquals(idealFaqValues.get(questionNumberInIdealList(questionNumber)),
+                getDriver()
+                        .findElement(questionLocator(questionNumber))
+                        .getText());
+        return this;
+    }
+
+    /**
+     * Метод клика по телу вопроса, чтобы открыть ответ
+     * @param questionNumber номер вопроса
+     */
+    public MainPage clickQuestionToOpenAnswer(int questionNumber){
+        getDriver().findElement(questionLocator(questionNumber)).click();
+        getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
+        return this;
+    }
+
+    /**
+     * Метод сверки полученного текста ответа с эталоном
+     * @param questionNumber номер вопроса
+     */
+    public MainPage realAnswerMatchesIdeal(int questionNumber){
+        Assert.assertEquals(idealFaqValues.get(answerNumberInIdealList(questionNumber)), getDriver().findElement(faqAnswerVisible).getText());
+        return this;
     }
 
     /**
@@ -201,5 +209,28 @@ public class MainPage {
      */
     private int answerNumberInIdealList(int questionNumber) {
         return (questionNumber * 2) - 1;
+    }
+
+    /**
+     * Получаем список вопросов в FAQ на сайте. Хотел сделать проверку пары с сайта с ключ/значением из эталона, но
+     * ассерт по хашмапе не заработал, не успеваю допилить
+     */
+    public MainPage checkTextFromFaqList() {
+        //Формируем список веб-элементов вопросов в FAQ
+        List<WebElement> elementsQuestions = getDriver().findElements(faqQuestions);
+        //Формируем текстовый список вопросов в FAQ
+        List<String> elementsQuestionsStrings = List.of(getDriver().findElement(faqQuestionsText).getText().split("\n"));
+        //Проверяем, что текст вопроса с сайта найден в нашем эталоне
+        elementsQuestionsStrings.forEach(el -> Assert.assertTrue(idealFaqValues.contains(el)));
+        //Цикл для всех веб-элементов вопросов FAQ: нажать на вопрос, дождаться отрисовки ответа, сверить ответ с эталоном
+        for (WebElement element : elementsQuestions) {
+            //Кликаем по элементу вопроса из FAQ
+            element.click();
+            //Ждём, когда элемент ответа после клика станет видим
+            getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(faqAnswerVisible));
+            //Проверяем, что текст ответа с сайта найден в нашем эталоне
+            Assert.assertTrue(idealFaqValues.contains(getDriver().findElement(faqAnswerVisible).getText()));
+        }
+        return this;
     }
 }
--- src/test/java/pageobject/pages/MainPageTest.java
+++ src/test/java/pageobject/pages/MainPageTest.java
@@ -5,18 +5,121 @@ import org.junit.Test;
 public class MainPageTest extends BaseTest {
     private final MainPage mainPage = new MainPage();
 
+    /**
+     * Тест работоспособности верхней кнопки "Заказать"
+     */
     @Test()
     public void clickOrderButtonTopSuccess() {
         mainPage
-                .open()
-                .orderButtonTopClick();
+                .openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .clickOrderButtonTop()
+                .checkIfOrderPageOpened();
     }
 
+    /**
+     * Тест работоспособности нижней кнопки "Заказать"
+     */
     @Test()
     public void clickOrderButtonBottomSuccess() {
         mainPage
-                .open()
-                .orderButtonBottomClick();
+                .openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .clickOrderButtonBottom()
+                .checkIfOrderPageOpened();
+    }
+
+    /**
+     * Тестирование FAQ согласно заданию
+     */
+    @Test()
+    public void firstFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(1)
+                .clickQuestionToOpenAnswer(1)
+                .realAnswerMatchesIdeal(1);
+    }
+
+    @Test()
+    public void secondFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(2)
+                .clickQuestionToOpenAnswer(2)
+                .realAnswerMatchesIdeal(2);
+    }
+
+    @Test()
+    public void thirdFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(3)
+                .clickQuestionToOpenAnswer(3)
+                .realAnswerMatchesIdeal(3);
+    }
+
+    @Test()
+    public void fourthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(4)
+                .clickQuestionToOpenAnswer(4)
+                .realAnswerMatchesIdeal(4);
+    }
+
+    @Test()
+    public void fifthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(5)
+                .clickQuestionToOpenAnswer(5)
+                .realAnswerMatchesIdeal(5);
+    }
+
+    @Test()
+    public void sixthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(6)
+                .clickQuestionToOpenAnswer(6)
+                .realAnswerMatchesIdeal(6);
+    }
+
+    @Test()
+    public void seventhFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(7)
+                .clickQuestionToOpenAnswer(7)
+                .realAnswerMatchesIdeal(7);
+    }
+
+    @Test()
+    public void eighthFaqQuestionAnswerCorrectSuccess() {
+        mainPage.openMainPage()
+                .clickAcceptCookieButton()
+                .checkIfMainPageOpened()
+                .scrollFaqIntoView()
+                .realQuestionMatchesIdeal(8)
+                .clickQuestionToOpenAnswer(8)
+                .realAnswerMatchesIdeal(8);
     }
 
     /**
@@ -25,58 +128,7 @@ public class MainPageTest extends BaseTest {
     @Test()
     public void textFaqQuestionAnswerCorrectSuccess() {
         mainPage
-                .open()
+                .openMainPage()
                 .checkTextFromFaqList();
-    }
-
-    /**
-     * Тестирование FAQ согласно заданию
-     */
-    @Test()
-    public void firstFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(1);
-    }
-
-    @Test()
-    public void secondFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(2);
-    }
-
-    @Test()
-    public void thirdFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(3);
-    }
-
-    @Test()
-    public void fourthFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(4);
-    }
-
-    @Test()
-    public void fifthFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(5);
-    }
-
-    @Test()
-    public void sixthFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(6);
-    }
-
-    @Test()
-    public void seventhFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(7);
-    }
-
-    @Test()
-    public void eighthFaqQuestionAnswerCorrectSuccess() {
-        mainPage.open()
-                .questionMatchesAnswer(8);
     }
 }
